08e26dd5fc5022545ee16469cfbe40b5ede88a3a
LLafforgue <ludovic.lafforgue.c@gmail.com>
2025-10-08 18:34:36 +0200

    #9 - Viminette - ft_upper & printable

---
+++ autres/ft_str_is_printable.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int ft_str_is_uppercase(char *str)
+{
+    if(*str == 0)
+        return (1);
+    else
+    {
+        if (*str >= 32 && *str <= 126)
+        {
+            if (*(str + 1))
+                return ft_str_is_uppercase(str + 1);
+            else
+                return (1);
+        }
+        else
+            return (0);   
+    }
+}
+
+int main(void)
+{
+    printf("La string contient est %d\n", ft_str_is_uppercase("125bla"));
+    printf("La string contient est %d\n", ft_str_is_uppercase("BOU"));
+    printf("La string contient est %d\n", ft_str_is_uppercase("BouBa"));
+    return (0);
+}
