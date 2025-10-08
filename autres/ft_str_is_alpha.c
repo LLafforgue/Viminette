02d2a01296b6d0e7096121ccdc58100eb97ecf2a
LLafforgue <ludovic.lafforgue.c@gmail.com>
2025-10-08 18:22:52 +0200

    #7 - Viminette - ft_num

--- autres/ft_str_is_alpha.c
+++ autres/ft_str_is_alpha.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
+
 int ft_str_is_alpha(char *str)
 {
-    if(!*str)
+    if(*str == 0)
         return (1);
     else
     {
-        if (*str >= ' ' || *str <= '~' + 1)
+        if ((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z'))
         {
             if (*(str + 1))
                 return ft_str_is_alpha(str + 1);
@@ -19,7 +20,7 @@ int ft_str_is_alpha(char *str)
 
 int main(void)
 {
-    printf("La string contient est %d\n", ft_str_is_alpha("Blabla"));
+    printf("La string contient est %d\n", ft_str_is_alpha("Bla&bla"));
     printf("La string contient est %d\n", ft_str_is_alpha(""));
     printf("La string contient est %d\n", ft_str_is_alpha(" "));
     return (0);
