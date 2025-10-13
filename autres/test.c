8d6b77a50cf3fc75738c5e7356b03b146c6ee5d6
LLafforgue <ludovic.lafforgue.c@gmail.com>
2025-10-13 22:54:06 +0200

    home split

--- autres/test.c
+++ autres/test.c
@@ -1,44 +1,70 @@
-#include <stdio.h>
+#include <stdlib.h>
 
-// int ft_fibonacci(int index)
-// {
-// 	int buff[2];
-
-// 	buff[0] = 1;
-// 	buff[1] = 1;
-// 	if (index < 0)
-// 		return -1;
-// 	if (index == 0)
-// 		return 0;
-// 	while(index > 2)
-// 	{
-// 		buff[index % 2] += buff[(index + 1) % 2];
-// 		index --;
-// 	}
-// 	return buff[(index + 1) % 2];
-// }
-
-int ft_fibonacci(int index)
-{
-	int x;
-
-	x = 1;
-	if (index < 0)
-		return -1;
-	if (index == 0)
-		return 0;
-	if (index > 2)
-		return ft_fibonacci(index - 1) + ft_fibonacci(index - 2);
-	
-	return x;
+int count_tokens(const char* str, char delim) {
+    int count = 0;
+    while (*str) {
+        if (*str == delim) count++;
+        str++;
+    }
+    return count + 1;
 }
 
-int main(void)
-{
-	int nbr;
-	nbr = ft_fibonacci(5);
-	printf("la fonction renvoie %d\n", nbr);
-	nbr = ft_fibonacci(4);
-	printf("la fonction renvoie %d\n", nbr);
-	return (0);
+char** split(const char* str, char delim, int* count) {
+    *count = count_tokens(str, delim);
+    char** result = malloc(*count * sizeof(char*));
+    if (!result) return NULL;
+
+    int i = 0;
+    const char* start = str;
+    while (*str) {
+        if (*str == delim) {
+            int len = str - start;
+            result[i] = malloc(len + 1);
+            if (!result[i]) {
+                for (int j = 0; j < i; j++) free(result[j]);
+                free(result);
+                *count = 0;
+                return NULL;
+            }
+            for (int j = 0; j < len; j++) result[i][j] = start[j];
+            result[i][len] = '\0';
+            i++;
+            start = str + 1;
+        }
+        str++;
+    }
+    // Dernier token
+    int len = str - start;
+    result[i] = malloc(len + 1);
+    if (!result[i]) {
+        for (int j = 0; j < i; j++) free(result[j]);
+        free(result);
+        *count = 0;
+        return NULL;
+    }
+    for (int j = 0; j < len; j++) result[i][j] = start[j];
+    result[i][len] = '\0';
+
+    return result;
+}
+
+void free_split(char** tokens, int count) {
+    for (int i = 0; i < count; i++) free(tokens[i]);
+    free(tokens);
+}
+
+int main() {
+    const char* str = "bonjour,tout,le,monde";
+    char delim = ',';
+    int count = 0;
+
+    char** tokens = split(str, delim, &count);
+    if (tokens) {
+        for (int i = 0; i < count; i++) {
+            // Utilisez tokens[i]
+            free(tokens[i]);
+        }
+        free(tokens);
+    }
+    return 0;
 }
